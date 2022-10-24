5d87c4ed00087cc65c1701daba13fb5491407da2
BeeBurn-IME <augusto.henrique12346@gmail.com>
2022-10-24 16:00:58 -0300

    Implementado o sistema de login

--- bughunter/__init__.py
+++ bughunter/__init__.py
@@ -5,12 +5,12 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-# app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bughunter.db'
+app.config['SECRET_KEY'] = '777439glc6c896ae2029594d'
 
-# db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
-# login_manager.login_view = 'login_page'
-# login_manager.login_message_category = "info"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = "info"
 from bughunter import routes
